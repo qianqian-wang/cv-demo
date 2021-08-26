6d2b891d6062fc2139c68d9ec5b178cd40c91d32
Queen <qianqianwang0112@163.com>
2021-08-26 11:25:13 +0800

    'viewport更改'

--- dist/main.dd042aa4.js
+++ dist/main.dd042aa4.js
@@ -159,7 +159,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54744' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '59789' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
